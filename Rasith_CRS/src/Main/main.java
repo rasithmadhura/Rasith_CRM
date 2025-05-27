80db3bea38762f6b5fee863934d9ce9bdc65a2db
rasithmadhura <rasithchandralal@gmail.com>
2025-05-27 22:14:54 +0530

    Creating the Folder Structure

---
+++ Rasith_CRS/src/Main/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author rasit
+ */
+public class main {
+
+public static void main( String []arg){
+    System.out.println("Hello world");
+}
+    
+}
